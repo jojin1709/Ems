35f5fc58ed358031743a0788dd2afd3395f4875a
jojin1709 <jojinn1709@gmail.com>
2026-04-27 14:23:13 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-//rfce type chyettu kitii (functional component kitii )
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div
+      style={{
+        marginTop: "70px", // space for fixed header
+        height: "450px",
+        background:
+          'url("https://www.shutterstock.com/image-photo/team-young-european-man-latin-600nw-2682189889.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    ></div>
+  );
 }
 
-export default Home
-
-
+export default Home;
